a0b93f3ab7885c91284ea5d4a6f2ce947a185004
luketevl <luketevl@gmail.com>
2016-12-23 18:31:09 -0200

    fix

--- server/main.js
+++ server/main.js
@@ -7,7 +7,7 @@ Meteor.startup(() => {
 // logiin
 Accounts.validateNewUser(user => {
   user.emails.forEach(email => {
-    if(email.address != 'gestaodepessoas@gattecnologia.com.br'){
+    if(email.address != 'gestaodepessoas@gattecnologia.com.br' && email.address != 'lhenrique@gattecnolgia.com.br'){
       throw new Meteor.Error(403, "Não possível criar conta com esse e-mail. Apenas o gestão de pessoas pode ter acesso");
     }
   });
